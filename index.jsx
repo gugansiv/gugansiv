dca62cb5e84c246bfa599d4b8c1e2c3fc8624378
gugan <39297317+gugansiv@users.noreply.github.com>
2024-01-30 00:31:24 +0000

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react'
 
-export const Frame = () => {
+import styles from './home.module.css'
+
+const Home = (props) => {
   return (
-    <div className="frame">
-      <div className="navbar">
-        <div className="text-wrapper">Home</div>
-        <div className="div">About</div>
-        <div className="text-wrapper-2">Skills</div>
-        <div className="text-wrapper-3">Projects</div>
-      </div>
-      <p className="portfolio">
-        <span className="span">Port</span>
-        <span className="text-wrapper-4">folio.</span>
-      </p>
-      <div className="text-wrapper-5">Contact</div>
+    <div className={styles['container']}>
+      <div className={styles['header']}></div>
+      <div className={styles['hero']}></div>
+      <img
+        src="https://images.unsplash.com/photo-1683009427540-c5bd6a32abf6?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwxfDF8YWxsfDF8fHx8fHwyfHwxNzA2NTc0NjI1fA&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1500"
+        alt="image"
+        className={styles['image']}
+      />
+      <div className={styles['features']}></div>
+      <div className={styles['pricing']}></div>
+      <div className={styles['banner']}></div>
+      <div className={styles['faq']}></div>
+      <div className={styles['footer']}></div>
     </div>
-  );
-};
+  )
+}
+
+export default Home
